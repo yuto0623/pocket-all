2ddb1a6df098493143921a52123db7d248cd4fe7
yuto0623 <yuto.ryr0623@gmail.com>
2025-03-11 17:21:31 +0900

    Update Card component title to include a badge placeholder

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -2,7 +2,10 @@ export default function Card() {
 	return (
 		<div className="card card-border">
 			<div className="card-body">
-				<h2 className="card-title">test Card</h2>
+				<h2 className="card-title">
+					test Card
+					{/* <div className="badge badge-neutral">Primary</div> */}
+				</h2>
 				<p>testsetstestse</p>
 				<div className="justify-end card-actions">
 					<button type="button" className="btn btn-primary">
