8245c26fe786b85d9b7ecf3110a9300949fce927
yuto0623 <yuto.ryr0623@gmail.com>
2025-03-11 16:33:08 +0900

    Add Card component and integrate multiple instances into Home layout

---
+++ app/components/Card/Card.tsx
@@ -0,0 +1,15 @@
+export default function Card() {
+	return (
+		<div className="card">
+			<div className="card-body">
+				<h2 className="card-title">test Card</h2>
+				<p>testsetstestse</p>
+				<div className="justify-end card-actions">
+					<button type="button" className="btn btn-primary">
+						Buy Now
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,30 @@
 import Image from "next/image";
 import Header from "./components/Header/Header";
+import Card from "./components/Card/Card";
 
 export default function Home() {
 	return (
 		<div>
 			<Header />
-			<button type="button" className="btn">
-				Default
-			</button>
+			<div className="flex flex-col gap-10">
+				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full max-w-[1600px] mx-auto">
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+				</div>
+				<button type="button" className="btn">
+					Default
+				</button>
+			</div>
 		</div>
 	);
 }
