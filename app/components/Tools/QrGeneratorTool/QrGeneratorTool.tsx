48e185c3a3dfa68e47cfde13ba9dad5cfe213f14
yuto0623 <yuto.ryr0623@gmail.com>
2025-03-12 10:55:49 +0900

    Update grid layout in QR generator tool for better responsiveness

--- app/components/Tools/QrGeneratorTool/QrGeneratorTool.tsx
+++ app/components/Tools/QrGeneratorTool/QrGeneratorTool.tsx
@@ -80,7 +80,7 @@ export default function QrGeneratorTool() {
 					</div>
 				</div>
 			</div>
-			<div className="grid grid-cols-2 gap-10">
+			<div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
 				<div>
 					<div className="card card-border">
 						<div className="card-body">
