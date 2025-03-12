8047fb4007f34b5195781fbd557bad8c807febb7
yuto0623 <yuto.ryr0623@gmail.com>
2025-03-12 09:50:13 +0900

    Add QR code generator tool page and update Card link in Home layout

---
+++ app/(tools)/qr-generator-tool/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+	return (
+		<div>
+			<h1>QRコード生成ツール</h1>
+			<p>QRコードを生成するツールです。</p>
+			<p>QRコードを生成するには、以下のボタンをクリックしてください。</p>
+		</div>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full max-w-[1600px] mx-auto mb-10">
 					<Card
 						title="QRコード作成"
-						href="/"
+						href="/qr-generator-tool"
 						image={
 							<Image
 								src="https://placehold.jp/928x548.png"
