f521e9e9ebd4bcfb152c0e42fdfe7dff6dec6790
yuto0623 <yuto.ryr0623@gmail.com>
2025-03-12 14:09:21 +0900

    Update image configuration to support remote patterns for external images

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
-		domains: ["placehold.jp"],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "placehold.jp",
+				port: "",
+				pathname: "/**",
+				search: "",
+			},
+		],
 	},
 };
 
